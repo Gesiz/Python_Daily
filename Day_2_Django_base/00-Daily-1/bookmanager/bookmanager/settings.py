845f54dd5aa5abd64c62baf6575bd1215e9d4a35
Gesiz <1040749858@qq.com>
2020-12-23 22:58:56 +0800

    注册book子应用

--- Day_2_Django_base/00-Daily-1/bookmanager/bookmanager/settings.py
+++ Day_2_Django_base/00-Daily-1/bookmanager/bookmanager/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'book.apps.BookConfig',
+    'book',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
