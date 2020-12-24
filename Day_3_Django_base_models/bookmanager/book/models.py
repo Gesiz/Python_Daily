9448b7592d3943aa498edf8062785015c27f6594
Gesiz <1040749858@qq.com>
2020-12-24 12:38:59 +0800

    添加迁移信息 准备生成响应数据库信息

--- Day_3_Django_base_models/bookmanager/book/models.py
+++ Day_3_Django_base_models/bookmanager/book/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class BookInfo(models.Model):
+    name = models.CharField(max_length=10)
+    pub_date = models.DateField(null=True)
+    readcount = models.IntegerField(default=0)
+    commentcount = models.IntegerField(default=0)
+    is_detele = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'bookinfo'
+        verbose_name = '图书信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    GENDER_CHOICE = (
+        (0, 'boy'),
+        (1, 'girl'),
+    )
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=0)
+    description = models.CharField(max_length=10)
+    is_delete = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'peopleinfo'
+        verbose_name = '人物信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
