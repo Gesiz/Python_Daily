29e72585b4ff08e68e09318d59476139bd5f6298
Gesiz <1040749858@qq.com>
2020-12-24 22:08:56 +0800

    添加 输入库插入数据方法

--- Day_3_Django_base_models/bookmanager/book/views.py
+++ Day_3_Django_base_models/bookmanager/book/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.urls import path
+from book.models import BookInfo
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("this is edge")
+
+
+
+# 数据库的增删改查 通过SHELL
+# python manage.py shell
+# 数据库的增加 两种方法
+# 1 . 手动调用 save 方法
+book  = BookInfo()
+book.name = 'django'
+book.readcount = 10
+book.commentcount = 100
+book.pub_date = '2002-2-2'
+book.save()
+# 2 . 通过中间对象创建
+BookInfo.objects.create(
+    name = 'python',
+    readcount = 10,
+    commentcount  = 100,
+    pub_date = '2002-2-2'
+)
