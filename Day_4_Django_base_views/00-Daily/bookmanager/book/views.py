a1842a18ee876b38d9a9a777ea1c6a4c9ae09807
Gesiz <1040749858@qq.com>
2020-12-27 00:48:45 +0800

    添加 view 改变路由表

--- Day_4_Django_base_views/00-Daily/bookmanager/book/views.py
+++ Day_4_Django_base_views/00-Daily/bookmanager/book/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('page')
+
+
+def book(request):
+    return HttpResponse('book')
+
+
+def login(request):
+    return HttpResponse('login')
+
+
+def weibo(request):
+    return HttpResponse('weibo')
