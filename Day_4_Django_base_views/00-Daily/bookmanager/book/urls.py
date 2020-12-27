6d93cc818baf1d79df7737c5633a946031b4d1e9
Gesiz <1040749858@qq.com>
2020-12-27 17:52:24 +0800

    注册路由

--- Day_4_Django_base_views/00-Daily/bookmanager/book/urls.py
+++ Day_4_Django_base_views/00-Daily/bookmanager/book/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
-from book.views import index, book, login, weibo
+from django.urls import path, register_converter
+from book.views import index, book, login, weibo,\
+    get_session,get_cookie,get_redirct,get_method,get_header,\
+    set_session,set_cookie,res_json,http_res
 
+from book.converters import MobileConverters
+
+register_converter(MobileConverters, 'phone')
 urlpatterns = [
     path('index/', index),
-    path('<cat_id>/<detail_id>/', book),
+    path('<cat_id>/<phone:detail_id>/', book),
     path('login/', login),
     path('weibo/', weibo),
 ]
--- Day_4_Django_base_views/00-Daily/bookmanager/book/views.py
+++ Day_4_Django_base_views/00-Daily/bookmanager/book/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from book.models import BookInfo, PeopleInfo
 
 
 # Create your views here.
@@ -8,13 +9,96 @@ def index(request):
     return HttpResponse('page')
 
 
-def book(request):
+def book(request, cat_id, detail_id):
+    # 关联查询
+
+    # 获取 ID = 2 书籍 中的人物信息
+    # 父类会根据外键关系 自动给我们的模型添加属性(字段)
+    # book = BookInfo.objects.get(id = cat_id)
+    # print(book.peopleinfo_set.all())
+
+    # 获取 ID = 6 人物的 书籍信息
+    # people = PeopleInfo.objects.get(id=cat_id)
+    # print(people.book,people.book.name,people.book.readcount)
+
+    # 关联过滤查询
+    # 查询 人物描述信息中 包含 八 的书籍名
+    # print(BookInfo.objects.filter(peopleinfo__description__contains='八'))
+
+    # 查询图书 去要求图书人物为 "郭靖"
+    print(BookInfo.objects.get(peopleinfo__name='郭靖'))
+    print(BookInfo.objects.get(peopleinfo__name__exact='郭靖'))
+
+    # 查询 图书 阅读量大于 30 的所有人物
+    print(PeopleInfo.objects.filter(book__readcount__gt=30))
+
+    # 查询书名为天龙八部的所有人物
+    print(PeopleInfo.objects.filter(book__name__exact='天龙八部'))
+
+    # QuerySet 可以被切片
+
+    # 分页
+    # 1 首先需要导入
+    from django.core.paginator import Paginator
+    # 2 查询结果集
+    people = PeopleInfo.objects.all().order_by('id')
+    # 3 创建分页类
+    paginator = Paginator(object_list=people, per_page=2)
+    persons = paginator.page(1)
+    # 获取分页
+    print("获取分页", persons.object_list)
+    # 获取分页数量
+    print("获取分页数量 ", paginator.num_pages)
+
     return HttpResponse('book')
 
 
 def login(request):
-    return HttpResponse('login')
+    query_string = request.GET
+    alist = query_string.getlist('key')
+
+    return HttpResponse(alist)
 
 
 def weibo(request):
-    return HttpResponse('weibo')
+    query_string = request.POST
+    alist = query_string.getlist('username')
+    b = query_string.get('asdasd', None)
+    return HttpResponse(b)
+
+
+def get_header(request):
+    head = request.META
+    return HttpResponse(head)
+
+
+def get_method(request):
+    return HttpResponse('')
+
+
+def http_res(request):
+    return HttpResponse('')
+
+
+def res_json(request):
+    return HttpResponse('')
+
+
+def get_redirct(request):
+    return HttpResponse('')
+
+
+def set_cookie(request):
+    return HttpResponse('')
+
+
+def get_cookie(request):
+    return HttpResponse('')
+
+
+def set_session(request):
+    return HttpResponse('')
+
+
+def get_session(request):
+    return HttpResponse('')
