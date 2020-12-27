4f26e47170aad20e80d42ecadd453e0a76bf401d
Gesiz <1040749858@qq.com>
2020-12-27 17:40:11 +0800

    修改路由表

--- Day_4_Django_base_views/00-Daily/bookmanager/book/urls.py
+++ Day_4_Django_base_views/00-Daily/bookmanager/book/urls.py
@@ -3,7 +3,7 @@ from book.views import index, book, login, weibo
 
 urlpatterns = [
     path('index/', index),
-    path('<cat_id>/<detail_id>', book),
+    path('<cat_id>/<detail_id>/', book),
     path('login/', login),
     path('weibo/', weibo),
 ]
