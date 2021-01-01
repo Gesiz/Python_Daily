b7e0ea355e4ff52c5e961ae27057827f073b5f07
Gesiz <1040749858@qq.com>
2021-01-02 00:28:48 +0800

    添加模型类 准备进行模型迁移

--- Day_2_Django_base/00-Daily-2/bookmanager/book/models.py
+++ Day_2_Django_base/00-Daily-2/bookmanager/book/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class BookInfo(models.Model):
+    # id django 会为我们自动创建一个主键
+    # name varchar(10)
+    name = models.CharField(max_length=10)
+
+    class Meta:
+        name = "bookinfo"
+        verbose_name_plural = verbose_name = "书籍信息"
+    # 人物
+
+
+class PeopleInfo(models.Model):
+    # id
+    # name
+    name = models.CharField(max_length=10)
+    # gender 性别
+    gender = models.BooleanField()
+    # book 外键
+    # 外键的级联关系
+    # 外键相关的知识 先自己回顾,我们在后天会将
+    # 外键在数据库中,系统会自动为我们添加一个 _id
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    class Meta:
+        name = "peopleinfo"
+        verbose_name_plural = verbose_name = "人物信息"
