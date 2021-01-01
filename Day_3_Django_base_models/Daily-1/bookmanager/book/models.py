931bb900208c4ac23516da048222a79ba0d06f5f
Gesiz <1040749858@qq.com>
2021-01-02 01:28:32 +0800

    修改

--- Day_3_Django_base_models/Daily-1/bookmanager/book/models.py
+++ Day_3_Django_base_models/Daily-1/bookmanager/book/models.py
@@ -7,7 +7,11 @@ from django.db import models
 class BookInfo(models.Model):
     # id django 会为我们自动创建一个主键
     # name varchar(10)
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=10, verbose_name='admin站点相关不是重点')
+    pub_date = models.DateField(null=True)
+    readcount = models.IntegerField(default=0)
+    commentcount = models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
@@ -19,16 +23,25 @@ class BookInfo(models.Model):
 
 
 class PeopleInfo(models.Model):
-    # id
-    # name
+    GENDER_CHOICE = (
+        (0, 'boy'),
+        (1, 'girl')
+    )
+    # (值,说明)
+    """
+    id
+    name 人物名
+    gender 性别
+    description 描述
+    is_delete 是否删除
+    book_id 书籍外键
+    """
     name = models.CharField(max_length=10)
-    # gender 性别
-    gender = models.BooleanField()
-    # book 外键
-    # 外键的级联关系
-    # 外键相关的知识 先自己回顾,我们在后天会将
-    # 外键在数据库中,系统会自动为我们添加一个 _id
-    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+    # choices 选项 只能从 元组中选取一个
+    # choices 的值 我们定义元组
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=0)
+    description = models.CharField(max_length=100, null=True)
+    is_delete = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -36,3 +49,5 @@ class PeopleInfo(models.Model):
     class Meta:
         db_table = "peopleinfo"
         verbose_name_plural = verbose_name = "人物信息"
+
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
--- Day_3_Django_base_models/Daily-1/bookmanager/bookmanager/settings.py
+++ Day_3_Django_base_models/Daily-1/bookmanager/bookmanager/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '9(y9xom7hqfbn266x8tisn8v!z1ht!1!3pv_@%5x58qzd0tvds'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -55,7 +53,7 @@ ROOT_URLCONF = 'bookmanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,17 +68,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bookmanager.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': '172.29.185.48',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD' : 'mysql',
+        'NAME': 'book_daily_01',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -100,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -114,8 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
