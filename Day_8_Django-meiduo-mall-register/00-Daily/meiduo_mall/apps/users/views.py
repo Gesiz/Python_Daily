70037754c2bc00f88a51279284a26feca3199eb2
Gesiz <1040749858@qq.com>
2021-01-03 02:07:30 +0800

    用户注册接口完善设计

--- Day_8_Django-meiduo-mall-register/00-Daily/meiduo_mall/apps/users/views.py
+++ Day_8_Django-meiduo-mall-register/00-Daily/meiduo_mall/apps/users/views.py
@@ -1,7 +1,10 @@
+import json
+
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views import View
 from apps.users.models import User
+import re
 
 
 # Create your views here.
@@ -31,10 +34,45 @@ class MobileCountView(View):
         count = User.objects.filter(mobile=mobile).count()
         return JsonResponse(request, {'code': 0, 'errmsg': 'ok', 'count': count})
 
+
 class RegisterView(View):
-    def post(self,request):
+    def post(self, request):
         """
         :param request:  请求对象
         :return:  注册结果
         """
-        pass
+        json_bytes = request.body
+        json_str = json_bytes.decode()
+        json_dict = json.loads(json_str)
+
+        # 提取参数
+        username = json_dict.get('username')
+        password = json_dict.get('password')
+        password2 = json_dict.get('password2')
+        mobile = json_dict.get('mobile')
+        allow = json_dict.get('allow')
+        sms_code = json_dict.get('sms_code')
+
+        # 判断参数是否齐全
+        if not all([username, password, password2, mobile, allow]):
+            return JsonResponse({'code': 400, 'errmsg': '缺少必传参数!'})
+        # 判断用户名是否是5-20个字符
+        if not re.match(r'^[a-zA-Z0-9_]{5,20}$', username):
+            return JsonResponse({'code': 400, 'errmsg': 'username格式有误!'})
+        # 判断密码是否是8-20个数字
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return JsonResponse({'code': 400, 'errmsg': 'password格式有误!'})
+        # 判断两次密码是否一致
+        if password != password2:
+            return JsonResponse({'code': 400, 'errmsg': '两次输入不对!'})
+        # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return JsonResponse({'code': 400, 'errmsg': 'mobile格式有误!'})
+        # 判断是否勾选用户协议
+        if allow != True:
+            return JsonResponse({'code': 400, 'errmsg': 'allow格式有误!'})
+        try:
+            user = User.objects.create_user(username=username, password=password, mobile=mobile)
+        except Exception as e :
+            return JsonResponse({'code': 400, 'errmsg': '注册失败'})
+
