3f50e752f3b5dc8cd74b0382c11c69ed7e198727
Gesiz <1040749858@qq.com>
2021-01-03 02:24:10 +0800

    修改错误 路由转换器 写反了

--- Day_8_Django-meiduo-mall-register/00-Daily/meiduo_mall/apps/users/urls.py
+++ Day_8_Django-meiduo-mall-register/00-Daily/meiduo_mall/apps/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from apps.users.views import MobileCountView, UsernameCountView,RegisterView
 
 urlpatterns = [
-    path('usernames/<uc:username>/count/', MobileCountView.as_view()),
-    path('mobiles/<mc:mobile>/count/', UsernameCountView.as_view()),
+    path('usernames/<uc:username>/count/', UsernameCountView.as_view()),
+    path('mobiles/<mc:mobile>/count/', MobileCountView.as_view()),
     path('register/',RegisterView.as_view())
 ]
