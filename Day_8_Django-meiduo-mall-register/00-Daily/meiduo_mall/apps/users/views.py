51cf149f379a3e40151a204f6ff39280b143bf35
Gesiz <1040749858@qq.com>
2021-01-03 01:44:02 +0800

    添加 判断用户名是否注册视图

--- Day_8_Django-meiduo-mall-register/00-Daily/meiduo_mall/apps/users/views.py
+++ Day_8_Django-meiduo-mall-register/00-Daily/meiduo_mall/apps/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from apps.users.models import User
+
 
 # Create your views here.
+
+class UsernameCountView(View):
+    """此处用来判断用户名是否注册"""
+
+    def get(self, request, username):
+        """
+        :param request:  请求对象
+        :param username:  请求参数
+        :return:
+        """
+        count = User.objects.filter(username=username).count()
+        return JsonResponse(request, {'code': 0, 'errmsg': 'ok', 'count': count})
