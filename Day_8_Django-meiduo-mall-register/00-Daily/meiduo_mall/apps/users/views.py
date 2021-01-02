04737dc7f69a707f41e000f22025f00a08ad7061
Gesiz <1040749858@qq.com>
2021-01-03 01:46:47 +0800

    添加 判断手机号是否注册视图

--- Day_8_Django-meiduo-mall-register/00-Daily/meiduo_mall/apps/users/views.py
+++ Day_8_Django-meiduo-mall-register/00-Daily/meiduo_mall/apps/users/views.py
@@ -13,7 +13,20 @@ class UsernameCountView(View):
         """
         :param request:  请求对象
         :param username:  请求参数
-        :return:
+        :return: JSON类型参数数据
         """
         count = User.objects.filter(username=username).count()
+        return JsonResponse(request, {'code': 0, 'errmsg': 'ok', 'count': count})
+
+
+class MobileCountView(View):
+    """此处用来判断手机号是否注册"""
+
+    def get(self, request, mobile):
+        """
+        :param request:  请求对象
+        :param mobile:  请求参数
+        :return: JSON类型参数数据
+        """
+        count = User.objects.filter(mobile=mobile).count()
         return JsonResponse(request, {'code': 0, 'errmsg': 'ok', 'count': count})
