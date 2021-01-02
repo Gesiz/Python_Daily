ce3b4c7c7fdc489c356990eb77408f272aa8b5a9
Gesiz <1040749858@qq.com>
2021-01-03 01:57:12 +0800

    添加路由文件 增加自定义路由

---
+++ Day_8_Django-meiduo-mall-register/00-Daily/meiduo_mall/apps/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from apps.users.views import MobileCountView,UsernameCountView
+
+
+urlpatterns = [
+    path('usernames/<uc:username>/count/',MobileCountView.as_view()),
+    path('mobiles/<mc:mobile>/count/',UsernameCountView.as_view()),
+]
