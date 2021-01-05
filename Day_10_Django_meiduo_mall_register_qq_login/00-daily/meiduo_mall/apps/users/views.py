a6e5dc82a7f0e336202f3a294bb3437e254e4ef6
Gesiz <1040749858@qq.com>
2021-01-05 21:51:32 +0800

    复习添加 功能 用户注册

--- Day_10_Django_meiduo_mall_register_qq_login/00-daily/meiduo_mall/apps/users/views.py
+++ Day_10_Django_meiduo_mall_register_qq_login/00-daily/meiduo_mall/apps/users/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from apps.users.models import User
+from django.http import JsonResponse
+from django.views import View
+
 
 # Create your views here.
+
+class UsernameCountView(View):
+    """判断用户名是否重复注册"""
+
+    def get(self, request, username):
+        """
+        :param request: 请求对象
+        :param username: 用户名
+        :return: JSON
+        """
+        count = User.objects.filter(username=username).count()
+        return JsonResponse({'code': 0, 'errmsg': 'OK', 'count': count})
+
+
+class MobileCountView(View):
+    """判断用户名是否重复注册"""
+
+    def get(self, request, mobile):
+        """
+        :param request: 请求对象
+        :param username: 用户名
+        :return: JSON
+        """
+        count = User.objects.filter(mobile=mobile).count()
+        return JsonResponse({'code': 0, 'errmsg': 'OK', 'count': count})
