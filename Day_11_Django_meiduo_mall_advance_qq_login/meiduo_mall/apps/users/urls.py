3eb4cca56dd748837df6ca9bd063c89894cfcee5
Gesiz <1040749858@qq.com>
2021-01-07 22:29:36 +0800

    创建 新练习 项目 创建子应用并完成 注册功能

---
+++ Day_11_Django_meiduo_mall_advance_qq_login/meiduo_mall/apps/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # 判断用户名是否重复
+    path('usernames/<username:username>/count/', views.UsernameCountView.as_view()),
+    path('mobiles/<mobile:mobile>/count/', views.MobileCountView.as_view()),
+    path('register/', views.RegisterView.as_view()),
+]
