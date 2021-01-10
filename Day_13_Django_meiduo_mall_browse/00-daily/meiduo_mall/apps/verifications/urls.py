2bbd6946dc22126a9b994d9d7306210d75be4afe
Gesiz <1040749858@qq.com>
2021-01-11 00:30:35 +0800

    完成短信验证码功能

--- Day_13_Django_meiduo_mall_browse/00-daily/meiduo_mall/apps/verifications/urls.py
+++ Day_13_Django_meiduo_mall_browse/00-daily/meiduo_mall/apps/verifications/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path, include
+from . import views
+
 urlpatterns = [
-    path('image_codes/<uuid>/',),
+    path('image_codes/<UUID:uuid>/', views.ImageCodeView.as_view()),
+    path('sms_codes/<mobile>/', views.SMSCodeView.as_view()),
+
 ]
